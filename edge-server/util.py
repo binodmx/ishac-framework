46ffe99da76c402dda6c411e10538b8b2e0658d8
binodmx <binod.16@cse.mrt.ac.lk>
2024-01-27 14:17:49 +1100

    Add end user implementation

--- edge-server/util.py
+++ edge-server/util.py
@@ -1,32 +1,14 @@
 import pickle
-from river import stream, tree
 import client
-import time
 
-# threshold = 0.77
-
-with open('../model.pkl', 'rb') as file:
+with open('model.pkl', 'rb') as file:
   clf = pickle.load(file)
 
-with open('../label_encoders.pkl', 'rb') as file:
-    label_encoders = pickle.load(file)
-
-def encode(x):
-    print(f"[INFO] Encoding data: {x}")
-    for key in x.keys():
-        x[key] = label_encoders[key].fit_transform(x[key])
-    print(f"[INFO] Encoded data: {x}")
-    return x
-
 def predict(x, y):
-    print(f"[INFO] Predicting for data: {x}, {y}")
-    encoded_x = encode(x)
-    y_pred = clf.predict_one(encoded_x)
-    clf.learn_one(encoded_x, y)
-    # y_prob = clf.predict_proba_one(encoded_x)
-    # if y_prob > threshold and y_pred != y:
-    #     return y_pred
-    # return y
+    print(f"[INFO] Predicting access level...")
+    y_pred = clf.predict_one(x)
+    clf.learn_one(x, y)
+    print(f"[INFO] Access level predicted")
     return y_pred
 
 def validate(data):
@@ -34,15 +16,12 @@ def validate(data):
     id = client.create_request("__ATTR_NAME__")
     attr_val = client.get_attribute(id)
     print(f"[INFO] Received attribute: {attr_val}")
-    x = data["x"]
-    y = data["y"]
-    print(f"[INFO] Predicting access level...")
-    # y_pred = predict(x, y)
-    # if y == 1:
-    #     if y_pred == 1:
-    #         return {"accessLevel":"GRANTED"}
-    #     return {"accessLevel":"SUSPICIOUS"}
-    # else:
-    #     if y_pred == 0:
-    #         return {"accessLevel":"DENIED"}
-    #     return {"accessLevel":"PENDING"}
+    y_pred = predict(data["x"], data["y"])
+    if data["y"] == 1:
+        if y_pred == 1:
+            return {"accessLevel":"GRANTED"}
+        return {"accessLevel":"SUSPICIOUS"}
+    else:
+        if y_pred == 0:
+            return {"accessLevel":"DENIED"}
+        return {"accessLevel":"PENDING"}
--- end-user/user.py
+++ end-user/user.py
@@ -1,12 +1,22 @@
 import requests
 import time
+import pandas as pd
+import json
+
+df_x = pd.read_csv('x.csv')
+df_y = pd.read_csv('y.csv')
+x = df_x.to_dict(orient='index')
+y = df_y.to_dict(orient='index')
 
 n = 10
-
 print(f"[INFO] Sending {n} requests to the server...")
 start = time.time()
 for i in range(n):
-    requests.get("http://localhost:5000")
+    url = "http://localhost:5000/validate"
+    headers = {"Content-Type": "application/json"}
+    payload = json.dumps({"x": x[i], "y": y[i]["allowed"]})
+    response = requests.post(url, headers=headers, data=payload)
+    print(response.json())
 end = time.time()
 print(f"[INFO] Total time for {n} requests: {end - start}")
 print(f"[INFO] Average time per request: {(end - start) / n}")
